685f9eefb0b302101a1945a824c15ddc7d5b91d6
Hanyuan Su <1102661278@qq.com>
2025-07-25 18:23:58 +0800

    Merge remote-tracking branch 'origin/main'

--- 2021国赛A题/Problem2.py
+++ 2021国赛A题/Problem2.py
@@ -12,7 +12,7 @@ Ry=np.array([[math.cos(b),0,-math.sin(b)],
             [0,1,0],
             [math.sin(b),0,math.cos(b)]])
 R0=Ry@Rz
-
+print(R0)
 #2.求天体坐标系下的顶点坐标
 A=np.array([[0,0,-300.79]]).T #大地坐标系下的顶点坐标
 A=np.linalg.inv(R0)@A
@@ -21,5 +21,21 @@ print(A)
 #3.计算应调整的索网节点
 R=300.4 #半径
 f=0.466*R+0.39 #焦距
+#读坐标
+df=pd.read_excel("merged.xlsx")
+x = df['X坐标（米）'].values
+y = df['Y坐标（米）'].values
+z = df['Z坐标（米）'].values
+mat = np.column_stack((x, y, z)).T
+print(mat)
 
+#旋转
+mat=R0@mat #天体坐标系下的主索节点坐标
+mat=mat.T
+print(mat)
+
+#筛选应该调整的主索节点
+def is_valid(coord):
+    x,y,z=coord
+    return
 
